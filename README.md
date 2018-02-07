27ecdbabf306c03de4931ceedf90f73d5dbb6c78
fredliu168 <fred@qzcool.com>
2018-02-07 12:40:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# 自动抓取经济学人文章和期刊
+
+
+使用Python3+BeautifulSoup自动下载经济学人期刊文章,保存成Markdown格式,可以使用MWeb等arkdown工具编辑方便阅读或者转换成word,pdf格式
+ 
+
+## 使用方法:
+
+> 运行环境: Python3 
+
+执行如下命令安装依赖：
+
+```python
+pip install -r requirements.txt
+``` 
+
+> 设置默认保存路径: SAVE_DIR = '/Users/fred/PycharmProjects/economist/'
+
+
+### 一.获取期刊
+
+传入期刊日期,可以自动下载该期刊,期刊查询地址:
+
+> https://www.economist.com/printedition/covers
+
+```python
+
+get_print_edition('2018-02-03')
+
+```
+
+### 二.根据URL获取单篇文章
+
+
+```python
+# article_url 文章网址
+# save_dir 保存路径,目录下需要创建图片文件夹,名称为images
+article_url='https://www.economist.com/news/science-and-technology/21736394-it-all-depends-which-palaeontologist-you-ask-strange-fossil-spider-or-maybe-not'
+save_dir = '/Users/fred/PycharmProjects/economist/'
+
+get_article_content(article_url,save_dir)
+
+```
+
+### 三.获取topics文章
+
+可以根据不同topics的url获取文章,自动保存到设置的默认目录下:
+
+```python
+# https://www.economist.com/sections/china
+get_tpoics_articles('https://www.economist.com/latest-updates')
+
+```
+
+欢迎关注我的公众号,有问题可以留言:
+
+![公众号](images/wechat-qcode.jpg )
+
+![赞赏](images/wx_shang.jpg)
+
+
+---
+
+# 下载的文章实例
+
+# The Falcon Heavy’s successful flight is another vindication for Elon Musk
+
+##### And another headache for SpaceX’s competitors
+
+![image](images/20180210_blp904_facebook.jpg)
+
+> Science and technology | Feb 5th 2018
+
+IT WAS not the most powerful launch ever seen at the Kennedy Space Cemtre’s Pad 39A; almost half a century ago the Apollo programme’s mighty Saturn V’s made use of it. But if the Falcon Heavy that took off from 39A on February 6th could boast only half the thrust of those bygone giants, its successful maiden voyage still proved it the most powerful rocket in the world today, as well as the most technologically advanced.
+
+Elon Musk, the founder of SpaceX, the firm which built the Falcon Heavy, had been frank about the possibility that the launch might fail, possibly spectacularly. The prospect of fireworks helped to entice the biggest crowd of spectators at Cape Canaveral since the heyday of the Space Shuttle, and an audience of half a million to a live webcast.
+
+...
---
+++ scrapy_main.py
@@ -0,0 +1,231 @@
+# -*- coding: UTF-8 -*-
+
+from bs4 import BeautifulSoup
+import requests
+import os
+
+'''
+使用爬虫获取经济学人期刊文章,自动保存成markdown格式,可以使用MWeb等markdown工具编辑方便阅读或者转换成word,pdf格式
+2018-02-07
+blog.qzcool.com
+by fredliu
+'''
+
+#保存文件的路径
+SAVE_DIR = '/Users/fred/PycharmProjects/economist/'
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 Sogousearch/Ios/5.9.8'
+    }
+
+
+def get_article_content(article_url,save_dir):
+    '''
+    获取单篇文章内容
+    :param artical_url: 文章路径
+    :param save_dir: 保存路径
+    :return:
+    '''
+
+    r = requests.get(article_url, headers=headers)
+
+    print(r.status_code)
+
+    #print(r.text)
+
+    html_doc = r.text
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    #print(soup.prettify())
+
+    # flytitle-and-title__title 标题
+    flytitle_and_title_title = soup.find("span", class_="flytitle-and-title__title")
+
+    f = open('{}/{}.md'.format(save_dir,flytitle_and_title_title.get_text()), 'w')
+
+    f.write("# {}".format(flytitle_and_title_title.get_text()))
+    f.write("\n\r")
+
+    #blog-post__rubric 子标题
+    post_rubric = soup.find("p", class_="blog-post__rubric")
+
+    if post_rubric != None:
+
+        f.write("##### {}".format(post_rubric.get_text()))
+        f.write("\n\r")
+
+
+
+    #获取图片
+    component_image_img= soup.find("img", class_="component-image__img blog-post__image-block")
+
+    if component_image_img != None:
+        #print(component_image_img.get('src'))
+        jpg_src = component_image_img.get('src')
+
+        url_path, img_name = os.path.split(jpg_src)
+
+        #print(img_name)
+
+        #print('{}/images/{}'.format(save_dir,img_name))
+
+        with open('{}/images/{}'.format(save_dir,img_name), 'wb') as file:
+            file.write(requests.get(jpg_src).content)
+
+        f.write("![image](images/{})".format(img_name))
+
+        f.write("\n\r")
+
+
+    # blog-post__section-link 文章分类
+    blog_post__section_link = soup.find("a", class_="blog-post__section-link")
+
+
+    #blog-post__datetime 时间
+    blog_post__datetime = soup.find("time", class_="blog-post__datetime")
+
+    if blog_post__section_link != None and blog_post__datetime != None:
+
+        f.write("> {} | {}".format(blog_post__section_link.get_text().replace('print-edition icon ',''),blog_post__datetime.get_text()))
+        f.write("\n\r")
+
+    elif blog_post__section_link != None:
+
+        f.write("> {}".format(blog_post__section_link.get_text().replace('print-edition icon ', '')))
+        f.write("\n\r")
+
+    elif blog_post__datetime != None:
+        f.write("> {}".format(blog_post__datetime.get_text()))
+        f.write("\n\r")
+
+    #获取文章内容
+    blog_post_text = soup.find("div", class_="blog-post__text")
+
+
+    for p in blog_post_text.find_all('p',class_=""):
+        f.write(p.get_text())
+        f.write("\n\r")
+
+    #f.write("Power by Fredliu (http://blog.qzcool.com)")
+    #f.write("\n\r")
+
+    f.close()
+
+
+def get_tpoics_articles(topics_url):
+    '''
+    获取专题文章
+    :param topics_url: https://www.economist.com/latest-updates
+    :return:
+    '''
+
+    # 创建topic目录
+    url_path, topic_name = os.path.split(topics_url)
+    # 保存文章的路径
+    article_dir = "{}/{}".format(SAVE_DIR, topic_name)
+    # 保存图片的目录
+    image_dir = "{}/{}/images".format(SAVE_DIR, topic_name)
+
+    mkdir(article_dir)
+    mkdir(image_dir)
+
+    r = requests.get(topics_url, headers=headers)
+    print(r.status_code)
+
+    html_doc = r.text
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    #print(soup.prettify())
+
+    #获取文章链接
+    for article_teaser in soup.find_all("article", class_="teaser"):
+
+        #print(article_teaser.prettify())
+
+        article_link = article_teaser.find("a")
+
+        print("正在下载文章{}".format(article_link.get('href')))
+
+        get_article_content('https://www.economist.com{}'.format(article_link.get('href')), article_dir)
+
+
+def get_print_edition(edition_number):
+    '''
+    获取期刊内容
+    :param edition_number: 刊物版本日期 2018-02-03
+    :return:
+    '''
+
+    r = requests.get('https://www.economist.com/printedition/{}'.format(edition_number), headers=headers)
+
+    print(r.status_code)
+
+    html_doc = r.text
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    #print(soup.prettify())
+
+    #创建文件目录
+    #保存文章的路径
+    article_dir = "{}/{}".format(SAVE_DIR,edition_number)
+    #保存图片的目录
+    image_dir = "{}/{}/images".format(SAVE_DIR,edition_number)
+
+    mkdir(article_dir)
+    mkdir(image_dir)
+
+    for article_link in soup.find_all("a", class_="link-button list__link"):
+
+        print("正在下载文章{}".format(article_link.get('href')))
+
+        get_article_content('https://www.economist.com{}'.format(article_link.get('href')),article_dir)
+
+
+def mkdir(path):
+    '''
+    创建文件夹目录
+    :param path:
+    :return:
+    '''
+    # 引入模块
+    import os
+
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        print(path + ' 创建成功')
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path + ' 目录已存在')
+        return False
+
+if __name__ == '__main__':
+
+    #artical_url ='https://www.economist.com/news/science-and-technology/21736394-it-all-depends-which-palaeontologist-you-ask-strange-fossil-spider-or-maybe-not'
+    #get_article_content('https://www.economist.com/news/world-week/21736202-politics-week','/Users/fred/PycharmProjects/economist/2018-02-03')
+    #pass
+
+    #get_print_edition('2018-02-03')
+    get_tpoics_articles('https://www.economist.com/latest-updates')
+
+
+
+
+
+
